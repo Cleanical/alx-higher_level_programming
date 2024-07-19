9519d1a07ef064aad7366ffba13f92c24a37c41f
Cleanical <miraclesunday72@gmail.com>
2024-07-19 20:37:38 +0100

    A script that prints a message depending on the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@
 
 const args = process.argv;
 if (args.length <= 2) {
-  console.log('No argument found');
+  console.log('No argument');
 } else if (args.length === 3) {
   console.log('Argument found');
 } else if (args.length >= 4) {
